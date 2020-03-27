7461440a91eef32374c43892fb98b67914572a52
Phill <borgosity@gmail.com>
2020-03-27 20:43:10 +1100

    refactor site content

--- resources/js/general.js
+++ resources/js/general.js
@@ -3,30 +3,30 @@ $(document).ready(function(){
 // page templates
 	$("#header").load("templates/header.html"); 
 	$("#parentheader").load("../../templates/header.html"); 
-	$("#menu").load("templates/menu.html"); 
-	$("#parentmenu").load("../../templates/menu.html"); 
 	$("#footer").load("templates/footer.html");
 	$("#parentfooter").load("../../templates/footer.html"); 
+	//$("#menu").load("templates/menu.html"); 
+	//$("#parentmenu").load("../../templates/menu.html"); 
 	
-// sticky menu
-	// Get the header
-	var header = document.getElementById("header");
-    if (header != null)
-    {
-	    // When the user scrolls the page, execute myFunction
-	    window.onscroll = function () { myFunction() };
+// // sticky menu
+// 	// Get the header
+// 	var header = document.getElementById("header");
+//     if (header != null)
+//     {
+// 	    // When the user scrolls the page, execute myFunction
+// 	    window.onscroll = function () { myFunction() };
 
 
-        // Add the sticky class to the header when you reach its scroll position. 
-        // Remove "sticky" when you leave the scroll position
-	    function myFunction() {
-		    // Get the offset position of the navbar
-		    if (window.pageYOffset >= header.offsetHeight) {
-			    menu.classList.add("sticky");
-		    } else {
-			    menu.classList.remove("sticky");
-		    }
-	    } 
-    }
+//         // Add the sticky class to the header when you reach its scroll position. 
+//         // Remove "sticky" when you leave the scroll position
+// 	    function myFunction() {
+// 		    // Get the offset position of the navbar
+// 		    if (window.pageYOffset >= header.offsetHeight) {
+// 			    menu.classList.add("sticky");
+// 		    } else {
+// 			    menu.classList.remove("sticky");
+// 		    }
+// 	    } 
+//     }
 });
 
