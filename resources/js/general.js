69ba51dba915b4e633043d12e0b8074cac8e77eb
Phill <borgosity@gmail.com>
2020-03-28 16:52:47 +1100

    add logo to menu bar

--- resources/js/general.js
+++ resources/js/general.js
@@ -5,28 +5,27 @@ $(document).ready(function(){
 	$("#parentheader").load("../../templates/header.html"); 
 	$("#footer").load("templates/footer.html");
 	$("#parentfooter").load("../../templates/footer.html"); 
-	//$("#menu").load("templates/menu.html"); 
-	//$("#parentmenu").load("../../templates/menu.html"); 
+	$("#menu").load("templates/menu.html"); 
+	$("#parentmenu").load("../../templates/menu.html"); 
 	
 // // sticky menu
-// 	// Get the header
-// 	var header = document.getElementById("header");
-//     if (header != null)
-//     {
-// 	    // When the user scrolls the page, execute myFunction
-// 	    window.onscroll = function () { myFunction() };
+	// Get the header
+	var header = document.getElementById("header");
+    if (header != null)
+    {
+	    // When the user scrolls the page, execute myFunction
+	    window.onscroll = function () { myFunction() };
 
-
-//         // Add the sticky class to the header when you reach its scroll position. 
-//         // Remove "sticky" when you leave the scroll position
-// 	    function myFunction() {
-// 		    // Get the offset position of the navbar
-// 		    if (window.pageYOffset >= header.offsetHeight) {
-// 			    menu.classList.add("sticky");
-// 		    } else {
-// 			    menu.classList.remove("sticky");
-// 		    }
-// 	    } 
-//     }
+        // Add the sticky class to the header when you reach its scroll position. 
+        // Remove "sticky" when you leave the scroll position
+	    function myFunction() {
+		    // Get the offset position of the navbar
+		    if (window.pageYOffset >= header.offsetHeight) {
+			    menu.classList.add("sticky");
+		    } else {
+			    menu.classList.remove("sticky");
+		    }
+	    } 
+    }
 });
 
